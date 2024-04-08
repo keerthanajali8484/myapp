70bc62f302ef6549339ba8da6d34ed507e391be5
keerthanajali <keerthanajali@gmail.com>
2024-04-08 20:59:36 +0530

    Updated add one to many association between recipe and chef

--- db/migrate/20240408055747_add_chef_id_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class AddChefIdToRecipes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :recipes,:chef_id,:integer
-  end
-end
---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  test "should get recipes index" do
+    get recipes_url
+    assert_response :success
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
